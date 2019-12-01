cb7a92cdfceb2dd57ac1d6ef03ac80043b055bb3
inar1 <inar1@protonmail.com>
2019-12-01 03:47:05 +0200

    update vim configurations

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-cd
-
 pip install flake8
 
 mkdir -p /home/${USER}/.vim/pack/flake8/start/
 git clone https://github.com/nvie/vim-flake8.git /home/${USER}/.vim/pack/flake8/start/vim-flake8
 
-echo ":source `pwd`/vimrc/.vimrc" > ~/.vimrc
+echo ":source `pwd`/inar1-vimrc" > ~/.vimrc
