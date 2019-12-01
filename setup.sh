a0aa30cfb1ec076592ed84644b1f26c022951395
inar1 <inar1@protonmail.com>
2019-12-01 03:24:16 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
+cd
+
 pip install flake8
 
 mkdir -p /home/${USER}/.vim/pack/flake8/start/
 git clone https://github.com/nvie/vim-flake8.git /home/${USER}/.vim/pack/flake8/start/vim-flake8
 
-echo ":source `pwd`/.vimrc" > ~/.vimrc
-
-echo "alias vim='vim -b'" > ~/.bash_aliases
-. ~/.bashrc
-
+echo ":source `pwd`/vimrc/.vimrc" > ~/.vimrc
