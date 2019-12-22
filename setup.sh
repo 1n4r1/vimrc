4bd9008cea94d970a63d899fdff161bd755e8177
inar1 <inar1@protonmail.com>
2019-12-22 12:27:20 +0200

    setup spell check and its dictionary

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
+cd
+
 pip install flake8
 
-mkdir -p /home/${USER}/.vim/pack/flake8/start/
-git clone https://github.com/nvie/vim-flake8.git /home/${USER}/.vim/pack/flake8/start/vim-flake8
+mkdir -p ~/.vim/pack/flake8/start/
+git clone https://github.com/nvie/vim-flake8.git ~/.vim/pack/flake8/start/vim-flake8
+mkdir -p ~/.vim/spell
+ln -s ~/vimrc/en.utf-8.add ~/.vim/spell/en.utf-8.add
+ln -s ~/vimrc/en.utf-8.add.spl ~/.vim/spell/en.utf-8.add.spl
 
-echo ":source `pwd`/inar1-vimrc" > ~/.vimrc
+echo ":source ~/vimrc/inar1-vimrc" > ~/.vimrc
