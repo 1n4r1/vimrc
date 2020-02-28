ee7ff8e752156adad89f9b71f83ca19cc1254315
inar1 <inar1@localhost.localdomain>
2020-02-28 13:22:35 +0200

    add lines for installing syntastic

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ pip install flake8
 
 mkdir -p ~/.vim/pack/flake8/start/
 git clone https://github.com/nvie/vim-flake8.git ~/.vim/pack/flake8/start/vim-flake8
+git clone https://github.com/vim-syntastic/syntastic.git ~/.vim/pack/syntastic/start/syntastic
 mkdir -p ~/.vim/spell
 ln -s ~/vimrc/en.utf-8.add ~/.vim/spell/en.utf-8.add
 ln -s ~/vimrc/en.utf-8.add.spl ~/.vim/spell/en.utf-8.add.spl
